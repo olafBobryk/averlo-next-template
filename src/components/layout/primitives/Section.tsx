c9c48f86e4d13fb000b450a62315ee3c29096313
olafBobryk <bobryk.olaf@gmail.com>
2026-01-10 21:33:43 +0100

    small fix

--- src/components/layout/primitives/Section.tsx
+++ src/components/layout/primitives/Section.tsx
@@ -18,7 +18,7 @@ const outerStyles = cva("w-full", {
 		},
 		height: {
 			auto: "h-auto",
-			hero: "h-svh max-h-[1000px]",
+			hero: "h-svh max-h-[1000px] min-h-fit",
 		},
 	},
 	defaultVariants: {
